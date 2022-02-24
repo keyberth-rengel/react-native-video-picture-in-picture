0ce811eb218dddaa47e06f6e6b93b4cca19dd07b
keyberth-rengel <keyberthdev@gmail.com>
2022-02-24 12:52:03 -0500

    [keyberth rengel][android] Ready the video playback and also the pip, PictureInPictureIcon option to show icon or text, only for android, TODO: IOS

--- android/src/main/java/com/reactnativevideopictureinpicture/VideoPictureInPictureModule.java
+++ android/src/main/java/com/reactnativevideopictureinpicture/VideoPictureInPictureModule.java
@@ -1,7 +1,13 @@
 package com.reactnativevideopictureinpicture;
 
-import androidx.annotation.NonNull;
+import android.app.PictureInPictureParams;
+import android.os.Build;
+import android.util.Rational;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -9,11 +15,29 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
 @ReactModule(name = VideoPictureInPictureModule.NAME)
-public class VideoPictureInPictureModule extends ReactContextBaseJavaModule {
+public class VideoPictureInPictureModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+
     public static final String NAME = "VideoPictureInPicture";
+
+    private final ReactApplicationContext reactContext;
+    private static final int ASPECT_WIDTH = 3;
+    private static final int ASPECT_HEIGHT = 4;
+    private boolean isPipSupported = false;
+    private boolean isCustomAspectRatioSupported = false;
+    private boolean isPipListenerEnabled = false;
+    private Rational aspectRatio;
 
     public VideoPictureInPictureModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
+        reactContext.addLifecycleEventListener(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            isPipSupported = true;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            isCustomAspectRatioSupported = true;
+            aspectRatio = new Rational(ASPECT_WIDTH, ASPECT_HEIGHT);
+        }
     }
 
     @Override
@@ -22,13 +46,45 @@ public class VideoPictureInPictureModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    public void enterPictureInPictureMode() {
+        if (isPipSupported) {
+            if (isCustomAspectRatioSupported) {
+              PictureInPictureParams params = new PictureInPictureParams.Builder()
+                        .setAspectRatio(this.aspectRatio).build();
+                getCurrentActivity().enterPictureInPictureMode(params);
+            } else
+                getCurrentActivity().enterPictureInPictureMode();
+        }
     }
 
-    public static native int nativeMultiply(int a, int b);
+    @ReactMethod
+    public void configureAspectRatio(Integer width, Integer height) {
+        aspectRatio = new Rational(width, height);
+    }
+
+    @ReactMethod
+    public void enableAutoPipSwitch() {
+        isPipListenerEnabled = true;
+    }
+
+    @ReactMethod
+    public void disableAutoPipSwitch() {
+        isPipListenerEnabled = false;
+    }
+
+    @Override
+    public void onHostResume() {
+    }
+
+    @Override
+    public void onHostPause() {
+        if (isPipSupported && isPipListenerEnabled) {
+            enterPictureInPictureMode();
+        }
+    }
+
+    @Override
+    public void onHostDestroy() {
+    }
 }
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,82 @@
-import * as React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-video-picture-in-picture';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  Platform,
+  Dimensions,
+} from 'react-native';
+import Video from 'react-native-video-picture-in-picture';
+
+// // Enter Pip mode
+// AndroidPip.enterPictureInPictureMode()
+
+// //Configure aspect ratio, works only on SDK version 26 and above
+// AndroidPip.configureAspectRatio(width, height)
+
+// // When enabled, PIP mode will be automatically entered when app is unfocused( User presses home, menu button etc)
+// AndroidPip.enableAutoPipSwitch()
+// AndroidPip.disableAutoPipSwitch()
+
+const { width } = Dimensions.get('screen');
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  let player = React.useRef();
+  const isDarkMode = useColorScheme() === 'dark';
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const onError = () => {};
+
+  const onLoad = () => {};
+
+  const onLoadStart = () => {
+    if (Platform.OS === 'ios') {
+    }
+  };
+
+  const VideoComponent = () => (
+    <Video
+      source={{
+        uri: 'https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8',
+        type: 'm3u8',
+      }}
+      onLoad={() => {
+        onLoad();
+      }}
+      repeat
+      volume={1}
+      allowsExternalPlayback
+      playInBackground={true}
+      // playWhenInactive={true}
+      rate={1.0}
+      // controls
+      onLoadStart={() => {
+        onLoadStart();
+      }}
+      ref={(ref) => {
+        player = ref;
+      }}
+      onError={onError} // Callback when video cannot be loaded
+      style={{
+        backgroundColor: 'black',
+        width: '100%',
+        height: '100%',
+      }}
+    />
+  );
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View
+      style={{
+        backgroundColor: isDarkMode ? '#000' : '#fff',
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+      }}
+    >
+      {VideoComponent()}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
